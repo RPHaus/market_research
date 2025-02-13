3db308fca4369da12d6ca9ab87ca31b2d4b48e6d
RPHaus <raffael.puttenhauser@gmail.com>
2025-02-13 23:10:27 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,13 @@ import pandas as pd
 import streamlit as st
 from api.twitter_api import fetch_twitter_trends
 from api.reddit_api import fetch_reddit_posts
+from decouple import config
 
-# API-Schlüssel und Tokens (diese solltest du sicher verwahren oder per .env-Datei einbinden)
-twitter_bearer_token = "DEIN_TWITTER_BEARER_TOKEN"
-reddit_client_id = "DEIN_REDDIT_CLIENT_ID"
-reddit_secret_token = "DEIN_REDDIT_SECRET_TOKEN"
+# API-Schlüssel aus der .env-Datei laden
+twitter_bearer_token = config("TWITTER_BEARER_TOKEN")
+reddit_client_id = config("REDDIT_CLIENT_ID")
+reddit_secret_token = config("REDDIT_SECRET_TOKEN")
+instagram_access_token = config("INSTAGRAM_ACCESS_TOKEN")
 
 # Daten abrufen
 twitter_data = fetch_twitter_trends(twitter_bearer_token)
